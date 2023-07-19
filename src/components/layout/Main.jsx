55939d7b9bb83f6c38944dff2b82058813eeab6c
haphong463 <111565442+haphong463@users.noreply.github.com>
2023-07-19 15:08:50 +0700

    'ok'

--- src/components/layout/Main.jsx
+++ src/components/layout/Main.jsx
@@ -1,14 +1,18 @@
 import React, { useState } from "react";
 import { Outlet } from "react-router-dom";
 import { MdTableRows } from "react-icons/md";
-import { IconName } from "react-icons/vsc"
+import { IconName } from "react-icons/vsc";
+
+import SvgExness from "../../assets/images/SvgExness";
 const Main = ({ children }) => {
   const [open, setOpen] = useState(false);
 
   return (
     <div className="mainPage">
       <div className="navbar">
-        <div className="openTab" onClick={() => setOpen(!open)}><MdTableRows color="#fff" size={30}/></div>
+        <div className="openTab" onClick={() => setOpen(!open)}>
+          <MdTableRows color="#fff" size={30} />
+        </div>
         <div></div>
       </div>
       <div
@@ -16,7 +20,10 @@ const Main = ({ children }) => {
       ></div>
       <div className={`slider ${open ? "slider--open" : "slider--close"}`}>
         <div className="menu">
-          <div className="tab--active">Kết nối đến Exness Partners</div>
+          <div className="tab--active">
+            <SvgExness />
+            Kết nối đến Exness Partners
+          </div>
         </div>
       </div>
       <div className="children-page">
