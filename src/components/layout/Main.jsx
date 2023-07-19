bfc89c2262ff9d6f7367bdcc98d5949edd0a0bd5
haphong463 <111565442+haphong463@users.noreply.github.com>
2023-07-19 13:08:53 +0700

    'ok'

--- src/components/layout/Main.jsx
+++ src/components/layout/Main.jsx
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { Outlet } from "react-router-dom";
-
+import { MdTableRows } from "react-icons/md";
+import { IconName } from "react-icons/vsc"
 const Main = ({ children }) => {
+  const [open, setOpen] = useState(false);
+
   return (
     <div className="mainPage">
-      <div className="navbar"></div>
-      <div className="slider">
+      <div className="navbar">
+        <div className="openTab" onClick={() => setOpen(!open)}><MdTableRows color="#fff" size={30}/></div>
+        <div></div>
+      </div>
+      <div
+        className={`navLayer ${open ? "navLayer--open" : "navLayer--close"}`}
+      ></div>
+      <div className={`slider ${open ? "slider--open" : "slider--close"}`}>
         <div className="menu">
           <div className="tab--active">Kết nối đến Exness Partners</div>
         </div>
