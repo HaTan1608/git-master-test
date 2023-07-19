a8eb9799690170080eb932d3cdd2b5be9c3843ac
haphong463 <111565442+haphong463@users.noreply.github.com>
2023-07-19 15:21:15 +0700

    'ok'

--- src/components/layout/Main.jsx
+++ src/components/layout/Main.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { Outlet } from "react-router-dom";
 import { MdTableRows } from "react-icons/md";
 import { IconName } from "react-icons/vsc";
-
+import brandImg1 from "../../assets/images/brand1.jpg";
 import SvgExness from "../../assets/images/SvgExness";
 const Main = ({ children }) => {
   const [open, setOpen] = useState(false);
@@ -13,7 +13,9 @@ const Main = ({ children }) => {
         <div className="openTab" onClick={() => setOpen(!open)}>
           <MdTableRows color="#fff" size={30} />
         </div>
-        <div></div>
+        <div className="img">
+          <img src={brandImg1} alt="" />
+        </div>
       </div>
       <div
         className={`navLayer ${open ? "navLayer--open" : "navLayer--close"}`}
